84b1055279e3e6c544fb7daf232b051ed8530cb8
Hassan Osama <hassasenbbbh@gmail.com>
2026-01-19 22:31:23 +0200

    fix: remove brackets from default imports in Allproviders

--- src/app/Allproviders/page.tsx
+++ src/app/Allproviders/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { PrayerProvider } from "../prayerTimes/PrayerTimeLogic/page";
-import { TasksProvider } from ".././tasks/tasksLogic/taskContext/page";
-import { ProjectsProvider } from "../projects/projectsLogic/page";
-import { PomodoroProvider } from "../pomodoro/pomodoroBox/PomodoroBoxLogic/page";
+import  PrayerProvider  from "../prayerTimes/PrayerTimeLogic/page";
+import  TasksProvider  from ".././tasks/tasksLogic/taskContext/page";
+import  ProjectsProvider  from "../projects/projectsLogic/page";
+import  PomodoroProvider  from "../pomodoro/pomodoroBox/PomodoroBoxLogic/page";
 import { HabitProvider } from "../habitTracker/habitTrackerLogic/habitsStore/page";
 import { FocusProvider } from "../focusMode/focusPage/focusModeLogic/page";
 
